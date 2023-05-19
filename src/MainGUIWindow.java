d067750f9b0feb57fcadd202b9a083e2829ebe31
Alp Senay <alps2789@nycstudents.net>
2023-05-19 10:59:27 -0400

    working...

---
+++ src/MainGUIWindow.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class MainGUIWindow extends JFrame {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JButton sendButton;
+    private JTextArea myTextArea;
+    private JButton clickMeForAButton;
+
+    public MainGUIWindow() {
+        createUIComponents();
+    }
+
+    private void createUIComponents() {
+        setContentPane(mainPanel);
+        setTitle("My GUI!");
+        setSize(900, 750);
+        setLocation(400,200);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
